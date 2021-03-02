4e24b6b329195efce8a2f6579687c031dac9cee0
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-03-02 18:32:16 -0500

    feat(register): connected register to backend

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import { FormControl, FormLabel, FormErrorMessage, Input, Box, Button } from "@chakra-ui/react";
+import {  Box, Button } from "@chakra-ui/react";
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../util/toErrorMap';
+import { useRouter } from "next/router"
 interface registerProps {
 
 }
 
-
-const Register: React.FC<registerProps> = ({ }) => {
+// workflow -> add graphql queries in the graphql folder (mutation or queries). -> run npm use gen to generate typescripts code in generated file -> use custom hooks to get response from server
+const Register: React.FC<registerProps> = ({}) => {
+    const [,register] = useRegisterMutation(); //first argument is the type of operation, second argument is our function
+    const router = useRouter();
     return (
         <div>
             <Wrapper variant = "small">
-                <Formik initialValues = {{username: "", password: ""}} onSubmit = {(values) => {console.log(values)}}>
+                {/* return a promise, stop spinning when promise is resolved */}
+                <Formik 
+                    initialValues = {{username: "", password: ""}} 
+                    onSubmit = { async (values, {setErrors}) => {
+                        const response = await register(values); 
+                        if (response.data?.register.errors){
+                            setErrors(toErrorMap(response.data.register.errors));
+                        } else if (response.data?.register.user){
+                            router.push("/");
+                        }
+                    }}
+                > 
                     {/* isSubmitting is a prop in Formik */}
                     {({isSubmitting}) => (
                         <Form>
@@ -20,7 +36,7 @@ const Register: React.FC<registerProps> = ({ }) => {
                                 <InputField name='username' placeholder="username" label="Username"/>
                                 <InputField name='password' placeholder="password" label="Password" type="password"/>
                             </Box>
-                            <Button mt={4} type="submit" variantColor="teal" isLoading={isSubmitting}> Register </Button>
+                            <Button mt={4} type="submit" variantcolor="teal" isLoading={isSubmitting}> Register </Button>
                         </Form>
                     )}
                 </Formik>
