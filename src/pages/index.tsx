2e9ec11929ea7306368ac6cb8a8fce3f43966cdb
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-03-22 16:38:24 -0400

    feat: updated landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,48 @@
-import { Link } from "@chakra-ui/layout";
-import { Spinner } from "@chakra-ui/react";
+import { Box, Heading, Stack, Text } from "@chakra-ui/layout";
+import { Button, Flex, Spinner } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../util/createUrqlClient";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{data}] = usePostsQuery({
-    variables: {
-      limit: 10
-    }
+  const [variables, setVariables] = useState({limit: 10, cursor: null as string | null})
+  const [{data, fetching}] = usePostsQuery({
+    variables
   });
+  
+  if (!fetching && !data){
+    console.log("Something went wrong", data);
+    return <div> Something went wrong. </div>
+  }
+  
   return (
     <Layout>
-      <NextLink href="/createPost">
-        <Link> Create Post </Link>
-      </NextLink>
-      {!data ? <Spinner /> : data.posts.map((p) => <div key={p.id}> {p.title} </div>)}
+      <Flex>
+        <Heading> New Posts </Heading>
+        <NextLink href="/createPost">
+          <Button ml="auto" mb={4}> Create Post </Button>
+        </NextLink>
+      </Flex>
+      {(!data && fetching) ? <Spinner /> : 
+        <Stack spacing={8} mb = {4}> 
+          {data!.posts.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px"> 
+              <Heading fontSize="xl"> {p.title} </Heading> 
+              <Text mt={4}> {p.textSnippet} </Text>
+            </Box>
+          ))} 
+        </Stack>}
+      {data && data.posts.hasMore ? <Flex>
+        <Button onClick={() => {
+          setVariables({
+            ...variables,
+            cursor: data.posts.posts[data.posts.posts.length - 1].createdAt, //set the cursor to be the last post createdAt and then request more from backend
+          }); 
+        }} mx="auto" my={8} isLoading={fetching}> Load more... </Button>
+      </Flex> : null}
     </Layout>
   )
 }
